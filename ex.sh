c5b2dc6ac59dcc7b832cd5cd7481231cb84f505f
UdyneO2 <dynemuhamad@gmail.com>
2024-11-23 04:52:27 +0800

    Update ex.sh

--- ex.sh
+++ ex.sh
@@ -1,1 +1,1 @@
-wget "https://drive.usercontent.google.com/download?id=12h7Aw04BsbJK4pQrayhHLZF9TI09i2F6&export=download&confirm=t&uuid=eb1848fc-a07a-43c5-931b-9aeeef7d09b3"
+mv 'download?id=12h7Aw04BsbJK4pQrayhHLZF9TI09i2F6&export=download&confirm=t&uuid=eb1848fc-a07a-43c5-931b-9aeeef7d09b3' rom.zip
