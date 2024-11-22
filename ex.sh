c854e6c0993cc6e921413d0f07c049a2fe6831c5
UdyneO2 <dynemuhamad@gmail.com>
2024-11-23 04:45:37 +0800

    Update ex.sh

--- ex.sh
+++ ex.sh
@@ -1,1 +1,1 @@
-wget "https://drive.google.com/file/d/12h7Aw04BsbJK4pQrayhHLZF9TI09i2F6/view"
+wget "https://drive.usercontent.google.com/download?id=12h7Aw04BsbJK4pQrayhHLZF9TI09i2F6&export=download&confirm=t&uuid=eb1848fc-a07a-43c5-931b-9aeeef7d09b3"
