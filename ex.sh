20bbaf49bdb7599d54eb567650aa97dd4c5629ca
UdyneO2 <dynemuhamad@gmail.com>
2024-11-23 05:48:32 +0800

    Update ex.sh

--- ex.sh
+++ ex.sh
@@ -1,1 +1,1 @@
-wget https://ava1.androidfilehost.com/dl/eC97U6B2EF_lIKiqlP9kJw/1732398031/14943124697586335764/Realme_C15_RMX2195_PU_11_OTA_A.27_OTA_%28by_oppostockrom.com%29.zip?
+wget https://ava6.androidfilehost.com/dl/jlEnGgAZhEJeNc0RlgFJYA/1732398457/14943124697586369234/%5BTuserHp.com%5D_RMX2195PU_11_A.33_210514_4db218d5.rar
